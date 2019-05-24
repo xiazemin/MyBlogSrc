27d7b37b37f0d45e85047599f4c61fefa17501d3
xiazemin <xiazemin@didichuxing.com>
2019-05-24 20:42:56 +0800

    new blog

--- _site/push.sh
+++ _site/push.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 jekyll build --trace
-git add *
+#git add *
+ls |xargs git add -f
 git commit -m 'new blog'
 git push https://github.com/xiazemin/MyBlogSrc.git master
 cd ./_site
-git add *
+#git add *
+ls |xargs git add -f
 git commit -m 'new blog'
 git push https://github.com/xiazemin/MyBlog.git master
 cd ..
