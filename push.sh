d5065c4818989367c1e3d7ca93046f00816325ef
xiazemin <xiazemin@didichuxing.com>
2019-05-26 16:12:50 +0800

    new blog  2019年 5月26日 星期日 16时12分50秒 CST

--- push.sh
+++ push.sh
@@ -2,7 +2,8 @@
 jekyll build --trace
 #git add *
 ls |xargs git add -f
-git commit -m 'new blog'
+t=`date`
+git commit -m "new blog  $t"
 git push https://github.com/xiazemin/MyBlogSrc.git master
 cd ./_site
 #git add *
